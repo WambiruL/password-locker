39c30d73a5a6df84d8baacfd494bb3673dedab3d
WambiruL <wambirulorraine@gmail.com>
2021-05-28 22:11:48 +0300

    Create attributes to the class credentials

--- passlock.py
+++ passlock.py
@@ -22,21 +22,31 @@ class User:
 
         User.user_list.append(self) 
 
+
 class Credentials:
     """
     Class that generates new instances of credentials
     """ 
 
     credentials_list=[]
+    
+    @classmethod
+    def user_exist(cls,username,password):
+        """
+        Method that checks if a user exists from the user_list
+        """
+        a_user=""
+        for user in User.user_list:
+            if (user.username==username and user.password==password):
+                a_user==user.username
+        return a_user
 
-@classmethod
-def user_exist(cls,username,password):
-    """
-    Method that checks if a user exists from the user_list
-    """
-    a_user=""
-    for user in User.user_list:
-        if (user.username==username and user.password==password):
-            a_user==user.username
-    return a_user
+    def __init__(self,account,username,password):
+        """
+        Method that defines user credentials to be stored
+        """
+
+        self.account=account
+        self.username=username
+        self.password=password
 
