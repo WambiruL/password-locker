8532bf1c17370bbcd7e51a1ec0af73ef2f25b57e
WambiruL <wambirulorraine@gmail.com>
2021-05-28 21:36:03 +0300

    Create a test case to test if object is initialised properly

--- test_passlock.py
+++ test_passlock.py
@@ -6,10 +6,18 @@ class TestUser(unittest.TestCase): #Unittest.Testcase helps in creating test cas
     Test class that defines test cases for the User class behaviours.
     """
 
-def setUp(self):
-    """
-    Method that runs before each test cases
-    """
+    def setUp(self):
+        """
+        Method that runs before each test cases
+        """
 
-    self.new_user= User("LorraineW", "Ziklag")
+        self.new_user= User("LorraineW", "Ziklag") #create user object
+
+    def test_init(self):
+        """
+        Test case to test if the object is initalised properly
+        """
+
+        self.assertEqual(self.new_user.username,"LorraineW")
+        self.assertEqual(self.new_user.password, "Ziklag")  #assertEqual checks for an expected result
 
