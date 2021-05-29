c0779f31ca5cb240353dfa2452df068c33439467
WambiruL <wambirulorraine@gmail.com>
2021-05-29 14:08:12 +0300

    Create the create user function

--- run.py
+++ run.py
@@ -1,3 +1,12 @@
-#!/usr/bin/env python3.6
-from passlock import Credentials
-from passlock import User
+#!/usr/bin/python3.6
+from passlock import User,Credentials
+
+def create_new_user(username,password):
+    """
+    Function to create a new user with a username and password
+    """
+
+    new_user=User(username,password)
+    return new_user
+
+
